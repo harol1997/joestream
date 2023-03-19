207cb2023f14a515f42bc846a2bcbccd23f1ef7d
Harol Alvarado <harolav3@gmail.com>
2023-03-19 05:28:02 -0500

    inicio de proyecto

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "asdasdeqd34134dsd12343dd"
+socketio = SocketIO(app)
+
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("save_file")
+def create_file(data):
+    socketio.emit("stream", data)
+    
